6f7f085b04e86313cb6e73780bd73de7520e37cd
myselfHimanshu <047himanshu@gmail.com>
2018-02-14 09:21:44 +0530

    module1 face recognition

--- Module1/face_recognition.py
+++ Module1/face_recognition.py
@@ -4,3 +4,8 @@
 import cv2
 
 #Loading the cascades
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
+
+#Defining the function that will do the detections
+
